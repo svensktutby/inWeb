a489f5fc28bbb6012854e45418feff5bd71a074c
svensktutby <svensk.tut.by@gmail.com>
2019-12-29 20:17:50 +0300

    slick-slider was styled

--- src/assets/js/components/slick.js
+++ src/assets/js/components/slick.js
@@ -1,7 +1,19 @@
-$('#portfolio').slick({
-  arrows: true,
+var portfolio = $('#portfolio');
+
+portfolio.slick({
+  arrows: false,
   dots: true,
   speed: 800,
   slidesToShow: 2,
   slidesToScroll: 2
+});
+
+$('#portfolio-arrow-prev').on('click', function (evt) {
+  evt.preventDefault();
+  portfolio.slick('slickPrev');
+});
+
+$('#portfolio-arrow-next').on('click', function (evt) {
+  evt.preventDefault();
+  portfolio.slick('slickNext');
 });
