5a816d17f273754d0bc6beebe5a30b4dd69250e7
svensktutby <svensk.tut.by@gmail.com>
2019-12-29 21:29:35 +0300

    some adaptive was added

--- src/assets/js/components/slick.js
+++ src/assets/js/components/slick.js
@@ -5,7 +5,17 @@ portfolio.slick({
   dots: true,
   speed: 800,
   slidesToShow: 2,
-  slidesToScroll: 2
+  slidesToScroll: 2,
+  responsive: [
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false
+      }
+    }
+  ]
 });
 
 $('#portfolio-arrow-prev').on('click', function (evt) {
